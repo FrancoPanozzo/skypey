ba71cdf397a58c13dada66eae47deb3f5d57c21a
Franco <francopanozzo@hotmail.com.ar>
2020-11-19 14:53:36 +0100

    Project files and directories set up, dependencies installed, basic components in place. FIRST COMMIT

--- src/index.js
+++ src/index.js
@@ -2,16 +2,23 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import store from './redux/store';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const render = () => {
+  fancyLog();
+  return ReactDOM.render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>,
+    document.getElementById('root')
+  );
+};
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+render();
+store.subscribe(render);
+
+// Function to log our store state when it changes
+function fancyLog() {
+  console.log('%c Rendered with 👉 👉👇', 'background: purple; color: #fff');
+  console.log(store.getState());
+}
